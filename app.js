b62ccb74e4a72581f0ddaf5cf2e1cbcdd140aff6
Lucas Souza <lucas_souza@outlook.com>
2022-09-01 15:12:15 -0300

    Full pipeline test

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 const port = 8080;
 
-app.get('/', (req, res) => res.send('DevOps starting!'));
+app.get('/', (req, res) => res.send('DevOps Full pipeline TEST!'));
 
 app.listen(port);
 console.log(`App running on http://localhost:${port}`);
